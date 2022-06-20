4cd8d41a1c22d6bb1745da811c3b365ecd098229
Rei Sugiura <reibomaru@gmail.com>
2022-06-20 14:53:16 +0900

    feat: add header content

--- client/web/src/components/contexts/Web3Provider/index.tsx
+++ client/web/src/components/contexts/Web3Provider/index.tsx
@@ -1,11 +1,141 @@
-import React from "react";
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import detectEthereumProvider from "@metamask/detect-provider";
+import Web3 from "web3";
+import { Contract } from "web3-eth-contract";
+import DSNS from "../../../contracts/DSNS.sol/DSNS.json";
+import {
+  CONTRACT_ADDRESS,
+  WEBSOCKET_HOST,
+  MUMBAI_CONTRACT_ADDRESS,
+  MUMBAI_WEBSOCKET_HOST,
+} from "../../../config";
 
-type props = {
+type web3Context = {
+  web3: Web3;
+  chainId: number;
+  contract: Contract;
+  account: string;
+  balance: string;
+  currencyUnit: string;
+  events: any;
+};
+
+const Web3Context = createContext<web3Context | null>(null);
+
+type web3ProviderProps = {
   children: React.ReactNode;
 };
 
-const Web3Provider = (props: props) => {
-  return <>{props.children}</>;
+const Web3Provider = (props: web3ProviderProps) => {
+  const [web3Obj, setWeb3Obj] = useState<web3Context | null>(null);
+
+  useEffect(() => {
+    window.ethereum.on("accountsChanged", () => {
+      // Handle the new accounts, or lack thereof.
+      // "accounts" will always be an array, but it can be empty.
+      window.location.reload();
+    });
+
+    window.ethereum.on("chainChanged", () => {
+      // Handle the new chain.
+      // Correctly handling chain changes can be complicated.
+      // We recommend reloading the page unless you have good reason not to.
+      window.location.reload();
+    });
+  }, []);
+
+  const selectContractAddress = useCallback((chainId: number) => {
+    if (chainId === 5) {
+      return CONTRACT_ADDRESS;
+    } else if (chainId === 80001) {
+      return MUMBAI_CONTRACT_ADDRESS;
+    } else if (chainId === 31337) {
+      return;
+    }
+  }, []);
+
+  const selectWebSocketHost = useCallback((chainId: number) => {
+    if (chainId === 5) {
+      return WEBSOCKET_HOST;
+    } else if (chainId === 80001) {
+      return MUMBAI_WEBSOCKET_HOST;
+    } else if (chainId === 31337) {
+      return "ws://127.0.0.1:8545";
+    }
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const provider = await detectEthereumProvider({ mustBeMetaMask: true });
+      if (provider && window.ethereum?.isMetaMask) {
+        console.log("Welcome to MetaMask User🎉");
+        const web3 = new Web3(Web3.givenProvider);
+        const chainId = await web3.eth.getChainId();
+        const contractAddress = selectContractAddress(chainId);
+        const webSocketHost = selectWebSocketHost(chainId);
+
+        const accounts = await web3.eth.requestAccounts();
+        const account = accounts[0];
+
+        const balance = await web3.eth.getBalance(accounts[0]);
+
+        const contract = new web3.eth.Contract(
+          DSNS.abi as any,
+          contractAddress
+        );
+
+        if (webSocketHost) {
+          const provider = new Web3.providers.WebsocketProvider(webSocketHost);
+          const web3EventListner = new Web3(provider);
+
+          const _contract = new web3EventListner.eth.Contract(
+            DSNS.abi as any,
+            CONTRACT_ADDRESS
+          );
+
+          const currencyUnit = chainId === 80001 ? "MATIC" : "ETH";
+
+          setWeb3Obj({
+            web3: web3,
+            chainId,
+            contract,
+            account,
+            balance: web3.utils.fromWei(balance),
+            currencyUnit,
+            events: _contract.events,
+          });
+        }
+      } else {
+        console.log("Please Install MetaMask🙇‍♂️");
+      }
+    })();
+  }, [selectContractAddress, selectWebSocketHost]);
+
+  return (
+    <>
+      {web3Obj ? (
+        <Web3Context.Provider value={web3Obj}>
+          {props.children}
+        </Web3Context.Provider>
+      ) : (
+        <p>読み込み中です</p>
+      )}
+    </>
+  );
 };
 
-export default React.memo(Web3Provider);
+export default Web3Provider;
+export const useWeb3 = () => {
+  const context = useContext(Web3Context);
+  if (context) {
+    return context;
+  } else {
+    throw new Error(`web3との接続に失敗しました。context is ${context}`);
+  }
+};
--- client/web/src/components/organisms/Header/index.tsx
+++ client/web/src/components/organisms/Header/index.tsx
@@ -1,7 +1,15 @@
+import { useWeb3 } from "../../contexts/Web3Provider";
+
 const Header = () => {
+  const { balance, account, currencyUnit } = useWeb3();
+
   return (
     <>
-      <h1>DSNS</h1>
+      <h1>dSNS</h1>
+      <p>your account: {account}</p>
+      <p>
+        your balance: {balance} {currencyUnit}
+      </p>
       <hr />
     </>
   );

fc486fc14e26a24ab4e34d8f1cba49dc64488181
Rei Sugiura <reibomaru@gmail.com>
2022-06-21 10:37:52 +0900

    feat: add link to header

--- client/web/src/components/organisms/Header/index.tsx
+++ client/web/src/components/organisms/Header/index.tsx
@@ -1,12 +1,34 @@
 import { useWeb3 } from "../../contexts/Web3Provider";
+import { useNavigate } from "react-router-dom";
+import { useCallback } from "react";
 
 const Header = () => {
   const { balance, account, currencyUnit } = useWeb3();
+  const navigate = useNavigate();
+
+  const navigateTo = useCallback(
+    (to: string) => {
+      return () => {
+        navigate(to);
+      };
+    },
+    [navigate]
+  );
 
   return (
     <>
-      <h1>dSNS</h1>
-      <p>your account: {account}</p>
+      <h1 onClick={navigateTo("/")} style={{ cursor: "pointer" }}>
+        dSNS
+      </h1>
+      <p>
+        your account:{" "}
+        <strong
+          onClick={navigateTo(`/${account}`)}
+          style={{ cursor: "pointer" }}
+        >
+          {account}
+        </strong>
+      </p>
       <p>
         your balance: {balance} {currencyUnit}
       </p>
